6fc22a671ee6b7afc81b6de82f54eb00683b8cd2
David Albert <davidbalbert@gmail.com>
2013-07-18 22:31:07 -0400

    Added authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ActiveRecord::Base
   has_many :accounts
 
   validates :nickname, presence: true
+
+  def self.authenticate_by_email(email, password)
+    account = Account.where(email: email).first
+    user = account.try(:user)
+    user.try(:authenticate, password) || false # forces return value to be false in all failure cases
+  end
 end
